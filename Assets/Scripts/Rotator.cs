10bf1b06e7cb4f9169f44394286e394d548601c0
Dilmer Valecillos <dilmervst@gmail.com>
2019-03-08 18:32:43 -0700

    Initial files

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 rotateSpeed;
+
+    void Update() => transform.Rotate(rotateSpeed * Time.deltaTime, Space.Self);    
+}
